45616541e1b168cfa8a84b1d65329c235040523e
xboston <bost56@gmail.com>
2013-04-30 13:32:43 +0600

    Примеры использования получения версии и валидации длины строки

--- examples/1.1.0/Phalcon_Validation_Validator_StringLength.php
+++ examples/1.1.0/Phalcon_Validation_Validator_StringLength.php
@@ -1,5 +1,93 @@
 <?php
 /**
- * Phalcon\Validation\Validator\StringLength
+ * Работа с валидатором длины строк Phalcon\Validation\Validator\StringLength
+ *
+ * Валидатор, при возможности, использует расширение mb_string, поддерживая работу с разными кодировками
+ * 
+ */
+
+
+/**
+ * Работа валидатора идёт через общий объект Phalcon\Validation
+ * 
+ */
+$validation = new Phalcon\Validation();
+
+/**
+ * Добавление правил валидации осуществляется методом add
+ * 
+ * В этом примере добавлен валидатор значения поля name, минимальная длина которого должна быть 5 символов
+ * Так же определено сообщение, которое будет выведено при несоответсвии правилу валидации
+ */
+$validation->add(
+    'name' ,
+    new Phalcon\Validation\Validator\StringLength(array(
+                                                       'min'            => 5 ,
+                                                       'messageMinimum' => 'Значение поля name слишком короткое' ,
+                                                  ))
+);
+
+/**
+ * Аналогичный пример, но для проверки на максимальный размер строки в 10 символов
+ */
+$validation->add(
+    'name2' ,
+    new Phalcon\Validation\Validator\StringLength(array(
+                                                       'max'            => 10 ,
+                                                       'messageMaximum' => 'Значение поля name2 слишком длинное'
+                                                  ))
+);
+
+/**
+ * Можно указывать сразу и минимальную и максимальную длину строки
+ */
+$validation->add(
+    'name3' ,
+    new Phalcon\Validation\Validator\StringLength(array(
+                                                       'min' => 5 ,
+                                                       'max' => 10 ,
+                                                  ))
+);
+
+/**
+ * Тексты сообщений при несоответсвии условиям длины строки так же можно указывать для каждого случая
+ */
+$validation->add(
+    'name3' ,
+    new Phalcon\Validation\Validator\StringLength(array(
+                                                       'min'            => 5 ,
+                                                       'max'            => 10 ,
+                                                       'messageMinimum' => 'Значение поля name4 слишком короткое' ,
+                                                       'messageMaximum' => 'Значение поля name4 слишком длинное'
+                                                  ))
+);
+
+/**
+ * Тестовый набор данных для валидации
+ */
+$fields = [
+    'name'  => 'Привет!' ,
+    'name2' => 'тут' ,
+    'name3' => 'идёт работа с валидатором' ,
+    'name4' => 'длины строк' ,
+
+];
+
+$messages = $validation->validate($fields);
+
+if ( count($messages) ) {
+
+    foreach ( $messages as $message ) {
+        echo $message , PHP_EOL;
+    }
+} else {
+
+    echo "Все строки соответсвуют условиям" , PHP_EOL;
+}
+
+/**
+ * Результат работы сценария будет примерно таким:
+ * - Value of field 'name3' exceeds the maximum 10 characters
+ * - Значение поля name4 слишком длинное
  * 
  */
--- examples/1.1.0/Phalcon_Version.php
+++ examples/1.1.0/Phalcon_Version.php
@@ -3,3 +3,11 @@
  * Phalcon\Version
  * 
  */
+
+// 1010023
+echo Phalcon\Version::getId();
+echo PHP_EOL;
+
+// 1.1.0 BETA 3
+echo Phalcon\Version::get();
+echo PHP_EOL;
