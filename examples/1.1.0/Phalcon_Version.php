1ecf72c0bfc7c9227ff43d1c6a3ccb025c521b2c
xboston <bost56@gmail.com>
2013-05-01 18:01:34 +0600

    Получение версии Phalcon

--- examples/1.1.0/Phalcon_Version.php
+++ examples/1.1.0/Phalcon_Version.php
@@ -1,13 +1,19 @@
 <?php
 /**
  * Phalcon\Version
- * 
+ * Выводит внутреннюю информацию о текущей версии ядра Phalcon
  */
 
-// 1010023
+/**
+ * Выводит идентификатор версии
+ * Пример: 1010023
+ */
 echo Phalcon\Version::getId();
 echo PHP_EOL;
 
-// 1.1.0 BETA 3
+/**
+ * Выводит название версии версии
+ * Пример: 1.1.0 BETA 3
+ */
 echo Phalcon\Version::get();
 echo PHP_EOL;
